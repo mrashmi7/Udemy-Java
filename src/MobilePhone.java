cdef76506b118dcb5cd6112219e3a1f8d37ed254
Diego Staphorst <diego-s-@hotmail.com>
2017-07-11 01:54:14 +0200

    change formatting inside MobilePhone class

--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -8,53 +7,11 @@ public class MobilePhone {
     private ContactList contactList = new ContactList();
     private Scanner scanner = new Scanner(System.in);
 
-
-    private void addContact() {
-        String name = getSearchInput("Give contact name: ");
-        String phoneNumber = getSearchInput("Give contact phonenumber: ");
-        if (!isDuplicated(name, phoneNumber))
-            contactList.add(new Contact(name, phoneNumber));
-    }
-
-    private boolean isDuplicated(String name, String phoneNumber) {
-        if (contactList.indexOf(name) >= 0 || contactList.indexOf(phoneNumber) >= 0) {
-            System.out.println("This name or phonenumber is already in the contactlist!");
-            return true;
-        }
-        return false;
-    }
-
-    private void printContacts() {
-        contactList.forEach((contact) -> System.out.printf("Contact: %s, %s\n", contact.getName(), contact.getPhoneNumber()));
-    }
-
-    private void deleteContact() {
-        int contact =  contactList.indexOf(getSearchInput("Give contact name: "));
-        if (contact >= 0)
-            contactList.remove(contact);
-    }
-
-    private void modifyContact() {
-        int contact =  contactList.indexOf(getSearchInput("Give contact name: "));
-        String newName = getSearchInput("Give new Contact name: ");
-        String newNumber = getSearchInput("Give new Contact phonenumber: ");
-        if (contact >= 0) {
-            contactList.get(contact).setName(newName);
-            contactList.get(contact).setPhoneNumber(newNumber);
-        }
-    }
-
-    private String getSearchInput(String message) {
-        System.out.print(message);
-        return scanner.next();
-    }
-
-
     public void openContactsMenu() {
         boolean opened = true;
         printOptions();
         while (opened) {
-            System.out.println(" ___________________________");
+            System.out.println();
             switch (getMenuOption()) {
                 case 1: // Show contacts
                     System.out.printf("Current amount of contacts in your mobile is %d\n", contactList.size());
@@ -98,6 +55,49 @@ public class MobilePhone {
 
     private int getMenuOption() {
         System.out.print("Select your option [1-6]: ");
-        return(scanner.nextInt());
+        return (scanner.nextInt());
+    }
+
+    private void printContacts() {
+        contactList.forEach((contact) -> System.out.printf("Contact: %s, Number: %s\n", contact.getName(), contact.getPhoneNumber()));
+    }
+
+
+    private void addContact() {
+        String name = getSearchInput("Give contact name: ");
+        String phoneNumber = getSearchInput("Give contact phonenumber: ");
+        if (!isDuplicated(name, phoneNumber))
+            contactList.add(new Contact(name, phoneNumber));
+    }
+
+    private boolean isDuplicated(String name, String phoneNumber) {
+        if (contactList.indexOf(name) >= 0 || contactList.indexOf(phoneNumber) >= 0) {
+            System.out.println("This name or phonenumber is already in the contactlist!");
+            return true;
+        }
+        return false;
+    }
+
+
+    private void modifyContact() {
+        int contact = contactList.indexOf(getSearchInput("Give contact name: "));
+        String newName = getSearchInput("Give new Contact name: ");
+        String newNumber = getSearchInput("Give new Contact phonenumber: ");
+        if (contact >= 0) {
+            contactList.get(contact).setName(newName);
+            contactList.get(contact).setPhoneNumber(newNumber);
+        }
+    }
+
+    private void deleteContact() {
+        int contact = contactList.indexOf(getSearchInput("Give contact name: "));
+        if (contact >= 0)
+            contactList.remove(contact);
+    }
+
+
+    private String getSearchInput(String message) {
+        System.out.print(message);
+        return scanner.next();
     }
 }
