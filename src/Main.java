9e1ee0115595df60757cdf6ef14f5db00f6fc80c
Diego Staphorst <diego-s-@hotmail.com>
2017-07-11 01:42:54 +0200

    link options to functions

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,6 @@ public class Main {
 //    private static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         MobilePhone newMobba = new MobilePhone();
-
-        newMobba.printContacts();
-        newMobba.addContact("Diego", "06-1231");
-        newMobba.printContacts();
-        newMobba.modifyContact("Diego", "Staphorst", "06-1231");
-        newMobba.printContacts();
-        newMobba.addContact("Diego1", "06-12311");
-        newMobba.addContact("Diego2", "06-12312");
-        newMobba.addContact("Diego3", "06-12313");
-        newMobba.addContact("Diego4", "06-12314");
-        newMobba.addContact("Diego5", "06-12315");
-        newMobba.printContacts();
-        newMobba.deleteContact("Staphorst1");
-        newMobba.printContacts();
         newMobba.openContactsMenu();
 
     }
--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -9,7 +9,9 @@ public class MobilePhone {
     private Scanner scanner = new Scanner(System.in);
 
 
-    public void addContact(String name, String phoneNumber) {
+    private void addContact() {
+        String name = getSearchInput("Give contact name: ");
+        String phoneNumber = getSearchInput("Give contact phonenumber: ");
         if (!isDuplicated(name, phoneNumber))
             contactList.add(new Contact(name, phoneNumber));
     }
@@ -22,54 +24,76 @@ public class MobilePhone {
         return false;
     }
 
-    public void printContacts() {
+    private void printContacts() {
         contactList.forEach((contact) -> System.out.printf("Contact: %s, %s\n", contact.getName(), contact.getPhoneNumber()));
     }
 
-    public void deleteContact(String searchValue) {
-        int contact =  contactList.indexOf(searchValue);
+    private void deleteContact() {
+        int contact =  contactList.indexOf(getSearchInput("Give contact name: "));
         if (contact >= 0)
             contactList.remove(contact);
     }
 
-    public void modifyContact(String searchValue, String newName, String newNumber) {
-        int contact =  contactList.indexOf(searchValue);
+    private void modifyContact() {
+        int contact =  contactList.indexOf(getSearchInput("Give contact name: "));
+        String newName = getSearchInput("Give new Contact name: ");
+        String newNumber = getSearchInput("Give new Contact phonenumber: ");
         if (contact >= 0) {
             contactList.get(contact).setName(newName);
             contactList.get(contact).setPhoneNumber(newNumber);
         }
     }
 
-    private String getSearchValue() {
-        return scanner.nextLine();
+    private String getSearchInput(String message) {
+        System.out.print(message);
+        return scanner.next();
     }
-    
+
+
     public void openContactsMenu() {
         boolean opened = true;
+        printOptions();
         while (opened) {
-            System.out.print("Choose your option: ");
+            System.out.println(" ___________________________");
             switch (getMenuOption()) {
                 case 1: // Show contacts
                     System.out.printf("Current amount of contacts in your mobile is %d\n", contactList.size());
                     printContacts();
                     break;
                 case 2: // Add contact
-                    System.out.println("Option 2: ");
+                    System.out.println("Add new contact!");
+                    addContact();
                     break;
                 case 3: // Modify contact
-                    System.out.println("Option 3: ");
+                    System.out.println("Modify existing contact: ");
+                    modifyContact();
                     break;
                 case 4: // Remove contact
-                    System.out.println("Option 4: ");
+                    System.out.println("Delete existing contact: ");
+                    deleteContact();
                     break;
                 case 5: // Print options
-                    System.out.println("Option 5: ");
+                    System.out.println("ContactList options:");
+                    printOptions();
                     break;
                 case 6: // Close contactlist
                     opened = false;
                     break;
             }
         }
+    }
+
+    private void printOptions() {
+        System.out.println(" ___________________________");
+        System.out.println("|                           |");
+        System.out.println("|   1 - Show all contacts   |");
+        System.out.println("|   2 - Add contact         |");
+        System.out.println("|   3 - Modify contact      |");
+        System.out.println("|   4 - Delete contact      |");
+        System.out.println("|   5 - Show options        |");
+        System.out.println("|   6 - Close ContactList   |");
+        System.out.println("|___________________________|");
+
     }
 
     private int getMenuOption() {
