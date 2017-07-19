c29f509109abd7c86cf5a875d39cc7ab2275d877
Diego Staphorst <diego-s-@hotmail.com>
2017-07-19 13:23:04 +0200

    Start with inner class challenge

--- src/innerClasses/Main.java
+++ src/innerClasses/Main.java
@@ -4,5 +4,19 @@ package innerClasses;
  * Created by diego on 17/07/2017.
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Gearbox mcLaren = new Gearbox(6);
+
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.operateClutch(true);
+
+        mcLaren.changeGear(2);
+        mcLaren.operateClutch(false);
+
+        System.out.println(mcLaren.wheelSpeed(2000));
+
+    }
 }
