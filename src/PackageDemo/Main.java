300f6c48f50c84b31b9d9864d0cb2bb211b5daff
Diego Staphorst <diego-s-@hotmail.com>
2017-07-31 14:17:22 +0200

    work on package challenge

---
+++ src/PackageDemo/Main.java
@@ -0,0 +1,11 @@
+package PackageDemo;
+
+/**
+ * Created by diego on 26/07/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
---
+++ src/PackageDemo/MyWindow.java
@@ -0,0 +1,32 @@
+package PackageDemo;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by diego on 26/07/2017.
+ */
+public class MyWindow extends Frame{
+    public MyWindow(String title) {
+        super(title);
+        setSize(500, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("The Complete Java Developer Course", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("By Diego Staphorst", 60, 100);
+    }
+}
